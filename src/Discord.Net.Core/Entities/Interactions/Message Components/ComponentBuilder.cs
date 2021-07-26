43cd6364f30c6f72afc93fd22f2da2cb5ec9875e
Nikon <anti-obolll@mail.ru>
2021-07-26 21:35:18 +0500

    makes button label argument optional

--- src/Discord.Net.Core/Entities/Interactions/Message Components/ComponentBuilder.cs
+++ src/Discord.Net.Core/Entities/Interactions/Message Components/ComponentBuilder.cs
@@ -132,7 +132,7 @@ namespace Discord
         /// <param name="row">The row the button should be placed on.</param>
         /// <returns>The current builder.</returns>
         public ComponentBuilder WithButton(
-            string label,
+            string label = null,
             string customId = null,
             ButtonStyle style = ButtonStyle.Primary,
             IEmote emote = null,
@@ -375,7 +375,7 @@ namespace Discord
         /// <param name="style">The custom ID of this button</param>
         /// <param name="emote">The emote of this button</param>
         /// <param name="disabled">Disabled this button or not</param>
-        public ButtonBuilder(string label, string customId = null, ButtonStyle style = ButtonStyle.Primary, string url = null, IEmote emote = null, bool disabled = false)
+        public ButtonBuilder(string label = null, string customId = null, ButtonStyle style = ButtonStyle.Primary, string url = null, IEmote emote = null, bool disabled = false)
         {
             this.CustomId = customId;
             this.Style = style;
