7c8319b853cf8f1da5264bc242f54ba431a4f45e
Simon Hjorthøj <sh2@live.dk>
2021-09-14 13:13:24 +0200

    Changed arguments to use the variables for errors

--- src/Discord.Net.Core/Entities/Interactions/SlashCommandBuilder.cs
+++ src/Discord.Net.Core/Entities/Interactions/SlashCommandBuilder.cs
@@ -296,7 +296,7 @@ namespace Discord
             set
             {
                 if (value?.Length > SlashCommandBuilder.MaxNameLength)
-                    throw new ArgumentException("Name length must be less than or equal to 32");
+                    throw new ArgumentException($"Name length must be less than or equal to {SlashCommandBuilder.MaxNameLength}");
                 if (value?.Length < 1)
                     throw new ArgumentException("Name length must at least 1 characters in length");
 
@@ -317,7 +317,7 @@ namespace Discord
             set
             {
                 if (value?.Length > SlashCommandBuilder.MaxDescriptionLength)
-                    throw new ArgumentException("Description length must be less than or equal to 100");
+                    throw new ArgumentException($"Description length must be less than or equal to {SlashCommandBuilder.MaxDescriptionLength}");
                 if (value?.Length < 1)
                     throw new ArgumentException("Name length must at least 1 character in length");
 
