9c0980b28930c3be99c174606aa292a78e1eee16
Cenk Ergen <57065323+Cenngo@users.noreply.github.com>
2021-09-25 00:24:00 +0300

    added sample application

---
+++ samples/04_interactions_framework/CommandHandler.cs
@@ -0,0 +1,165 @@
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_interactions_framework
+{
+    public class CommandHandler
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly InteractionService _commands;
+        private readonly IServiceProvider _services;
+
+        public CommandHandler(DiscordSocketClient client, InteractionService commands, IServiceProvider services)
+        {
+            _client = client;
+            _commands = commands;
+            _services = services;
+        }
+
+        public async Task InitializeAsync ( )
+        {
+            // Add the public modules that inherit InteractionModuleBase<T> to the InteractionService
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+
+            // Process the InteractionCreated payloads to execute Interactions commands
+            _client.InteractionCreated += HandleInteraction;
+
+            // Process the command execution results 
+            _commands.SlashCommandExecuted += SlashCommandExecuted;
+            _commands.ContextCommandExecuted += ContextCommandExecuted;
+            _commands.ComponentCommandExecuted += ComponentCommandExecuted;
+        }
+
+        private Task ComponentCommandExecuted (ComponentCommandInfo arg1, Discord.IInteractionCommandContext arg2, IResult arg3)
+        {
+            if (!arg3.IsSuccess)
+            {
+                switch (arg3.Error)
+                {
+                    case InteractionCommandError.UnmetPrecondition:
+                        // implement
+                        break;
+                    case InteractionCommandError.UnknownCommand:
+                        // implement
+                        break;
+                    case InteractionCommandError.BadArgs:
+                        // implement
+                        break;
+                    case InteractionCommandError.Exception:
+                        // implement
+                        break;
+                    case InteractionCommandError.Unsuccessful:
+                        // implement
+                        break;
+                    default:
+                        break;
+                }
+            }    
+
+            return Task.CompletedTask;
+        }
+
+        private Task ContextCommandExecuted (ContextCommandInfo arg1, Discord.IInteractionCommandContext arg2, IResult arg3)
+        {
+            if (!arg3.IsSuccess)
+            {
+                switch (arg3.Error)
+                {
+                    case InteractionCommandError.UnmetPrecondition:
+                        // implement
+                        break;
+                    case InteractionCommandError.UnknownCommand:
+                        // implement
+                        break;
+                    case InteractionCommandError.BadArgs:
+                        // implement
+                        break;
+                    case InteractionCommandError.Exception:
+                        // implement
+                        break;
+                    case InteractionCommandError.Unsuccessful:
+                        // implement
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private Task SlashCommandExecuted (SlashCommandInfo arg1, Discord.IInteractionCommandContext arg2, IResult arg3)
+        {
+            if (!arg3.IsSuccess)
+            {
+                switch (arg3.Error)
+                {
+                    case InteractionCommandError.UnmetPrecondition:
+                        // implement
+                        break;
+                    case InteractionCommandError.UnknownCommand:
+                        // implement
+                        break;
+                    case InteractionCommandError.BadArgs:
+                        // implement
+                        break;
+                    case InteractionCommandError.Exception:
+                        // implement
+                        break;
+                    case InteractionCommandError.Unsuccessful:
+                        // implement
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private async Task HandleInteraction (SocketInteraction arg)
+        {
+            try
+            {
+                // Create an execution context that matches the generic type parameter of your InteractionModuleBase<T> modules
+                var ctx = new SocketInteractionCommandContext(_client, arg);
+                switch (arg)
+                {
+                    // To execute a Slash Command Handler you need to get the string[] containing the names of the Slash Command, Sub-Slash Command Group and Sub-Slash Command.
+                    // You can use the GetCommandKeywords() extension method to obtain this array.
+                    case SocketSlashCommand command:
+                        await _commands.ExecuteSlashCommandAsync(ctx, command.GetCommandKeywords(), _services);
+                        break;
+                    // To execute a Component Interaction Handler, you need to input the CustomId of the component. (Keep in mind that, URL components don't raise interaction events)
+                    case SocketMessageComponent component:
+                        await _commands.ExecuteComponentCommandAsync(ctx, component.Data.CustomId, _services);
+                        break;
+                    // Context command handlers can be executed using a single method, in this case, ExecuteContextCommandAsync method expects you to provide the Name of the Context
+                    // command.
+                    case SocketUserCommand:
+                    case SocketMessageCommand:
+                        await _commands.ExecuteContextCommandAsync(ctx, arg.Data.Name, _services);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+
+                // If a Slash Command execution fails it is most likely that the original interaction acknowledgement will persist. It is a good idea to delete the original
+                // response, or at least let the user know that something went wrong during the command execution.
+                if(arg.Type == InteractionType.ApplicationCommand)
+                    await arg.GetOriginalResponseAsync().ContinueWith(async (msg) => await msg.Result.DeleteAsync());
+            }
+        }
+    }
+}
