6c6cb4ce1f3c3457c24477f0ace1eaba84e20a69
Cenk Ergen <57065323+Cenngo@users.noreply.github.com>
2021-09-27 16:28:04 +0300

    added indentations to typeconverter docs

--- src/Discord.Net.Interactions/TypeConverters/TypeConverter.cs
+++ src/Discord.Net.Interactions/TypeConverters/TypeConverter.cs
@@ -5,25 +5,25 @@ using System.Threading.Tasks;
 namespace Discord.Interactions
 {
     /// <summary>
-    /// Base class for creating TypeConverters. <see cref="InteractionService"/> uses TypeConverters to interface with Slash Command parameters
+    ///     Base class for creating TypeConverters. <see cref="InteractionService"/> uses TypeConverters to interface with Slash Command parameters
     /// </summary>
     public abstract class TypeConverter
     {
         /// <summary>
-        /// Will be used to search for alternative TypeConverters whenever the Command Service encounters an unknown parameter type
+        ///     Will be used to search for alternative TypeConverters whenever the Command Service encounters an unknown parameter type
         /// </summary>
         /// <param name="type"></param>
         /// <returns></returns>
         public abstract bool CanConvertTo (Type type);
 
         /// <summary>
-        /// Will be used to get the Application Command Option type
+        ///     Will be used to get the Application Command Option type
         /// </summary>
         /// <returns>The option type</returns>
         public abstract ApplicationCommandOptionType GetDiscordType ();
 
         /// <summary>
-        /// Will be used to read the incoming payload before executing the method body
+        ///     Will be used to read the incoming payload before executing the method body
         /// </summary>
         /// <param name="context">Command exexution context</param>
         /// <param name="option">Recieved option payload</param>
@@ -32,7 +32,7 @@ namespace Discord.Interactions
         public abstract Task<TypeConverterResult> ReadAsync (IInteractionCommandContext context, SocketSlashCommandDataOption option, IServiceProvider services);
 
         /// <summary>
-        /// Will be used to manipulate the outgoing command option, before the command gets registered to Discord
+        ///     Will be used to manipulate the outgoing command option, before the command gets registered to Discord
         /// </summary>
         /// <param name="properties"></param>
         public virtual void Write(ApplicationCommandOptionProperties properties) { }
@@ -41,6 +41,7 @@ namespace Discord.Interactions
     /// <inheritdoc/>
     public abstract class TypeConverter<T> : TypeConverter
     {
+        /// <inheritdoc/>
         public sealed override bool CanConvertTo (Type type) =>
             typeof(T).IsAssignableFrom(type);
     }
