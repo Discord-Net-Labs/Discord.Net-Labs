6ddba5e8177ac90538bc39eaf3d5d567966be2db
Emily <89871431+emillly-b@users.noreply.github.com>
2021-10-02 00:43:03 -0500

    Generate Classes based on PR

---
+++ src/Discord.Net.Rest/API/Exceptions/GenericJsonException.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord.Rest.API
+{
+    //Visual studio wont allow 2FAException??
+    class MFAException
+    {
+    }
+    class UserSearchException
+    {
+    }
+    class ReactionException
+    {
+    }
+    class APIException
+    {
+    }
+    class StageException
+    {        
+    }
+}
