a95b48677a9c382d19ba1749258fc8892945c7f3
Emily <89871431+emillly-b@users.noreply.github.com>
2021-10-03 09:32:26 -0500

    Begin parser work

--- src/Discord.Net.Rest/Utils/DiscordErrorParser.cs
+++ src/Discord.Net.Rest/Utils/DiscordErrorParser.cs
@@ -1,4 +1,5 @@
 using Discord.API;
+using Discord.Rest.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,26 @@ namespace Discord.Rest
     {
         public static void Parse(string outgoingJson, DiscordError error)
         {
-
+            if (error.Code == 0)
+                throw new Exception();
+            if (!int.TryParse((error.Code + "").Substring(0, 2), out var errorPrefix))
+                throw new Exception("Somthing has gone terribly wrong");
+            throw errorPrefix switch
+            {
+                10 => new UnknownEntitieException(), 
+                20 => new GeneralActionException(),
+                30 => new LimitReachedException(),
+                40 => new GeneralRequestException(outgoingJson, error),
+                50 => new PrecondionException(),
+                60 => new MFAException(),
+                80 => new UserSearchException(),
+                90 => new ReactionException(),
+                13 => new APIException(),
+                15 => new StageException(),
+                16 => new ReplyAndThreadException(),
+                17 => new StickerException(),
+                _ => new Exception("not implemented"),
+            };
         }
     }
 }
