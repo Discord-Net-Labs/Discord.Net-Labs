4840c66865b5f36c4c7d5429ae7a0fed2e728d47
Cat <lumitydev@gmail.com>
2021-10-27 17:36:20 -0500

    Update SocketChannel.cs

--- src/Discord.Net.WebSocket/Entities/Channels/SocketChannel.cs
+++ src/Discord.Net.WebSocket/Entities/Channels/SocketChannel.cs
@@ -31,7 +31,6 @@ namespace Discord.WebSocket
         /// <exception cref="InvalidOperationException">Unexpected channel type is created.</exception>
         internal static ISocketPrivateChannel CreatePrivate(DiscordSocketClient discord, ClientState state, Model model)
         {
-            Console.WriteLine(model.Type);
             return model.Type switch
             {
                 ChannelType.DM => SocketDMChannel.Create(discord, state, model),
