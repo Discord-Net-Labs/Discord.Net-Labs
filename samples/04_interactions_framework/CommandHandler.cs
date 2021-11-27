f7a9785decb3b37c0f50d83486ea9018d294e0cf
Cenk Ergen <57065323+Cenngo@users.noreply.github.com>
2021-11-27 16:30:08 +0300

    Revert "update interactions sample app"
    This reverts commit 6ac8cd0da60b440874cf29abc7202cdc49fd0538.

--- samples/04_interactions_framework/CommandHandler.cs
+++ samples/04_interactions_framework/CommandHandler.cs
@@ -138,6 +138,8 @@ namespace _04_interactions_framework
 
                 // If a Slash Command execution fails it is most likely that the original interaction acknowledgement will persist. It is a good idea to delete the original
                 // response, or at least let the user know that something went wrong during the command execution.
+                if(arg.Type == InteractionType.ApplicationCommand)
+                    await arg.GetOriginalResponseAsync().ContinueWith(async (msg) => await msg.Result.DeleteAsync());
             }
         }
     }
