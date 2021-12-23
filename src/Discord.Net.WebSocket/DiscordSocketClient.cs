Discord-Net-Labs/Discord.Net-Labs

commit ee35acc7d0423e41570d0451607c6bf7c4f3fbea

Add guild parameter to UserLeftEvent (#373)
* Ensure User Left Event

* Add guild parameter to user left event

src/Discord.Net.WebSocket/DiscordSocketClient.cs
@@ -1309,10 +1309,14 @@ namespace Discord.WebSocket
                                             return;
                                         }
 
-                                        if(user == null)
+                                        user = State.GetUser(data.User.Id);
+
+                                        if (user != null)
+                                            user.Update(State, data.User);
+                                        else
                                             user = SocketGlobalUser.Create(this, State, data.User);
 
-                                        await TimedInvokeAsync(_userLeftEvent, nameof(UserLeft), user).ConfigureAwait(false);
+                                        await TimedInvokeAsync(_userLeftEvent, nameof(UserLeft), guild, user).ConfigureAwait(false);
                                     }
                                     else
                                     {

src/Discord.Net.WebSocket/BaseSocketClient.Events.cs
@@ -424,12 +424,12 @@ namespace Discord.WebSocket
         }
         internal readonly AsyncEvent<Func<SocketGuildUser, Task>> _userJoinedEvent = new AsyncEvent<Func<SocketGuildUser, Task>>();
         /// <summary> Fired when a user leaves a guild. </summary>
-        public event Func<SocketUser, Task> UserLeft
+        public event Func<SocketGuild, SocketUser, Task> UserLeft
         {
             add { _userLeftEvent.Add(value); }
             remove { _userLeftEvent.Remove(value); }
         }
-        internal readonly AsyncEvent<Func<SocketUser, Task>> _userLeftEvent = new AsyncEvent<Func<SocketUser, Task>>();
+        internal readonly AsyncEvent<Func<SocketGuild, SocketUser, Task>> _userLeftEvent = new AsyncEvent<Func<SocketGuild, SocketUser, Task>>();
         /// <summary> Fired when a user is banned from a guild. </summary>
         public event Func<SocketUser, SocketGuild, Task> UserBanned
         {

src/Discord.Net.WebSocket/DiscordShardedClient.cs
@@ -445,7 +445,7 @@ namespace Discord.WebSocket
             client.GuildUpdated += (oldGuild, newGuild) => _guildUpdatedEvent.InvokeAsync(oldGuild, newGuild);
 
             client.UserJoined += (user) => _userJoinedEvent.InvokeAsync(user);
-            client.UserLeft += (user) => _userLeftEvent.InvokeAsync(user);
+            client.UserLeft += (guild, user) => _userLeftEvent.InvokeAsync(guild, user);
             client.UserBanned += (user, guild) => _userBannedEvent.InvokeAsync(user, guild);
             client.UserUnbanned += (user, guild) => _userUnbannedEvent.InvokeAsync(user, guild);
             client.UserUpdated += (oldUser, newUser) => _userUpdatedEvent.InvokeAsync(oldUser, newUser);

commit 868c76b837b24dc319434bc032b1dc7d0c04bee6

Fix SocketGuildUser being changed to SocketGlobalUser in UserLeft
* Regression from ee35acc7d0423e41570d0451607c6bf7c4f3fbea

src/Discord.Net.WebSocket/DiscordSocketClient.cs
@@ -519,7 +519,7 @@ namespace Discord.WebSocket
             if(model == null)
                 return null;
 
-            
+
             if (model.GuildId.IsSpecified)
             {
                 var guild = State.GetGuild(model.GuildId.Value);
@@ -1309,7 +1309,7 @@ namespace Discord.WebSocket
                                             return;
                                         }
 
-                                        user = State.GetUser(data.User.Id);
+                                        user ??= State.GetUser(data.User.Id);
 
                                         if (user != null)
                                             user.Update(State, data.User);
@@ -2100,7 +2100,7 @@ namespace Discord.WebSocket
                                             {
                                                 await TimedInvokeAsync(_speakerRemoved, nameof(SpeakerRemoved), stage, guildUser);
                                             }
-                                        }    
+                                        }
                                     }
 
                                     await TimedInvokeAsync(_userVoiceStateUpdatedEvent, nameof(UserVoiceStateUpdated), user, before, after).ConfigureAwait(false);
@@ -2491,7 +2491,7 @@ namespace Discord.WebSocket
                                 }
 
                                 break;
-                            case "THREAD_MEMBERS_UPDATE": 
+                            case "THREAD_MEMBERS_UPDATE":
                                 {
                                     await _gatewayLogger.DebugAsync("Received Dispatch (THREAD_MEMBERS_UPDATE)").ConfigureAwait(false);
 