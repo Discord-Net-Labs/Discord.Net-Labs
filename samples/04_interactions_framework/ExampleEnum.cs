4f869f1b35988d3174e1479128071fd9e656fd4a
Xeno <eliotd@gmail.com>
2021-12-27 12:58:50 -0800

    update docs/examples to include enum Display sample

--- samples/04_interactions_framework/ExampleEnum.cs
+++ samples/04_interactions_framework/ExampleEnum.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace _04_interactions_framework
         First,
         Second,
         Third,
-        Fourth
+        Fourth,
+        [Display(Name = "Twenty First")]
+        TwentyFirst
     }
 }
