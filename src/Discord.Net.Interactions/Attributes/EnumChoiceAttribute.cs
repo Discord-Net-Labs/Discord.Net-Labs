c97cb4843411526e581d07c6b890e490e3b59047
Xeno <eliotd@gmail.com>
2021-12-28 07:08:49 -0800

    Update EnumChoiceAttribute.cs

--- src/Discord.Net.Interactions/Attributes/EnumChoiceAttribute.cs
+++ src/Discord.Net.Interactions/Attributes/EnumChoiceAttribute.cs
@@ -3,7 +3,7 @@ using System;
 namespace Discord.Interactions
 {
     /// <summary>
-    ///     Customize the displayed value of a slash command choice enum.
+    ///     Customize the displayed value of a slash command choice enum. Only works with the default enum type converter.
     /// </summary>
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
     public class ChoiceDisplayAttribute : Attribute
