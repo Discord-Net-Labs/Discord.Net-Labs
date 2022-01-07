a40629fcd4d3589dc0c0d94f8c06b4d6d995faa1
Cenk Ergen <57065323+Cenngo@users.noreply.github.com>
2022-01-08 01:30:05 +0300

    solve build errors

--- src/Discord.Net.Rest/Entities/Interactions/Modals/RestModal.cs
+++ src/Discord.Net.Rest/Entities/Interactions/Modals/RestModal.cs
@@ -33,8 +33,6 @@ namespace Discord.Rest
             return entity;
         }
 
-        /// <inheritdoc/>
-        public override bool HasResponded { get; internal set; }
         private object _lock = new object();
 
         /// <summary>
