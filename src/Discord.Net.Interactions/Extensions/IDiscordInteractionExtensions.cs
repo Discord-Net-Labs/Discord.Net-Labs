e942041aa5a1cdcce86bfb239f59402efd64b04c
Cenk Ergen <57065323+Cenngo@users.noreply.github.com>
2022-01-15 02:07:51 +0300

    update RespondWithModalAsync method

--- src/Discord.Net.Interactions/Extensions/IDiscordInteractionExtensions.cs
+++ src/Discord.Net.Interactions/Extensions/IDiscordInteractionExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 
 namespace Discord.Interactions
@@ -7,23 +8,31 @@ namespace Discord.Interactions
         /// <summary>
         ///     Respond to an interaction with a <see cref="IModal"/>.
         /// </summary>
+        /// <typeparam name="T">Type of the <see cref="IModal"/> implementation.</typeparam>
         /// <param name="interaction">The interaction to respond to.</param>
-        /// <param name="modal">The modal to respond with.</param>
         /// <param name="options">The request options for this <see langword="async"/> request.</param>
         /// <returns>A task that represents the asynchronous operation of responding to the interaction.</returns>
         public static async Task RespondWithModalAsync<T>(this IDiscordInteraction interaction, string customId, RequestOptions options = null)
             where T : class, IModal
         {
-            var modalInfo = ModalUtils.GetOrAdd(typeof(T));
+            if (!ModalUtils.TryGet<T>(out var modalInfo))
+                throw new ArgumentException($"{typeof(T).FullName} isn't referenced by any registered Modal Interaction Command and doesn't have a cached {typeof(ModalInfo)}");
 
             var builder = new ModalBuilder()
                 .WithCustomId(customId)
                 .WithTitle(modalInfo.Title);
 
-            foreach (var textInput in modalInfo.TextComponents)
-                builder.AddTextInput(textInput.Label, textInput.CustomId, textInput.Style, textInput.Placeholder, textInput.MinLength, textInput.MaxLength, textInput.IsRequired, textInput.DefaultValue as string);
+            foreach(var input in modalInfo.Components)
+                switch (input)
+                {
+                    case TextInputComponentInfo textComponent:
+                        builder.AddTextInput(textComponent.Label, textComponent.CustomId, textComponent.Style, textComponent.Placeholder, textComponent.MinLength, textComponent.MaxLength, textComponent.IsRequired, textComponent.DefaultValue as string);
+                        break;
+                    default:
+                        throw new InvalidOperationException($"{input.GetType().FullName} isn't a valid component info class");
+                }
 
-            await interaction.RespondWithModalAsync(builder.Build(), options);
+            await interaction.RespondWithModalAsync(builder.Build(), options).ConfigureAwait(false);
         }
     }
 }
