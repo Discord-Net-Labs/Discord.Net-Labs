5547c47e7cbf7cb05cb8d9eac5c6be50b069f0c4
Quin Lynch <49576606+quinchs@users.noreply.github.com>
2022-02-08 20:15:51 -0400

    Update src/Discord.Net.Core/DiscordErrorCode.cs

--- src/Discord.Net.Core/DiscordErrorCode.cs
+++ src/Discord.Net.Core/DiscordErrorCode.cs
@@ -95,7 +95,7 @@ namespace Discord
         #endregion
 
         #region General Request Errors (40XXX)
-        MaximumNumberOfEditsToMessagesOlderThan1HourReached.TryAgainLater = 30046,
+        MaximumNumberOfEditsPerHourReached = 30046,
         TokenUnauthorized = 40001,
         InvalidVerification = 40002,
         OpeningDMTooFast = 40003,
