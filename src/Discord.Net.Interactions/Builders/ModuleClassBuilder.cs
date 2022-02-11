2b2c5c3722ee4e6244f39408271594617954392b
Cenk Ergen <57065323+Cenngo@users.noreply.github.com>
2022-02-11 12:56:42 +0300

    add inline docs

--- src/Discord.Net.Interactions/Builders/ModuleClassBuilder.cs
+++ src/Discord.Net.Interactions/Builders/ModuleClassBuilder.cs
@@ -465,8 +465,7 @@ namespace Discord.Interactions.Builders
 
         private static void BuildComponentParameter(ComponentCommandParameterBuilder builder, ParameterInfo paramInfo, bool isComponentParam)
         {
-            builder.SetAsRouteSegment(!isComponentParam);
-
+            builder.SetIsRouteSegment(!isComponentParam);
             BuildParameter(builder, paramInfo);
         }
 
